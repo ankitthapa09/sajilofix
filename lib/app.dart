9ca353774db7eb2476ec523829e70a15c300797e
suyalthapa09@gmail.com <suyalthapa09@gmail.com>
2025-11-25 15:41:27 +0545

    created splash screen

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SajiloFix extends StatelessWidget {
+  const SajiloFix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: SajiloFix());
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sajilo FIx"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SafeArea(child: Column(children: [
+
+        ],
+      )),
+    );
+  }
+}
