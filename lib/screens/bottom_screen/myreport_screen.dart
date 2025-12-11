199ef83806a8eb6bd149dee8578536e91bf6977f
suyalthapa09@gmail.com <suyalthapa09@gmail.com>
2025-12-11 10:10:55 +0545

    Bottom navigation screens like report screen, myreport screen and profile screen created

---
+++ lib/screens/bottom_screen/myreport_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyreportScreen extends StatefulWidget {
+  const MyreportScreen({super.key});
+
+  @override
+  State<MyreportScreen> createState() => _MyreportScreenState();
+}
+
+class _MyreportScreenState extends State<MyreportScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
